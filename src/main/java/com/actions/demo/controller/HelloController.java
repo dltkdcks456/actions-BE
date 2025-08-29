4bdf0ec8db871c46c5de6d251f55466f79388fd8
dltkdcks456 <sangchan0825@gmail.com>
2025-08-29 20:56:24 +0900

    sdf

---
+++ src/main/java/com/actions/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.actions.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello,world";
+    }
+}
